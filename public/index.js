87cc48f0e3c9022bbe05ea5959e4ca9a79696e8d
foxcube3 <109860475+foxcube3@users.noreply.github.com>
2022-10-23 00:57:25 -0600

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,33 +1,33 @@
+#// Define libraries
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
-
+#// Define app & server
 const app = express();
 const expressServer = http.createServer(app);
-
 app.use(express.json());
 app.use(express.static('public'));
-
+#// Invoke defined server
 const io = new Server(expressServer);
-
+#// Print server-side when user connects
 io.on('connect', function (socket) {
     console.log('a user is connected');
-
+#// Serve realtime serverside clock
     setInterval(function () {
         let date = new Date().toLocaleTimeString()
         socket.send(date)
     }, 1000)
-
+#// Print server-side when user disconnects
     socket.on('disconnect', () => {
         console.log('user disconnected.')
     })
 
 })
-
+#// Serve primary HTML
 app.get('/', (req, res, next) => {
     res.render('index.html');
 })
-
-expressServer.listen(4000, () => {
-    console.log('server is listening on port 4000.')
+#// Server listens for requests at FQDN or IP:8080
+expressServer.listen(8080, () => {
+    console.log('server is listening on port 8080.')
 })
